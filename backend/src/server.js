a5e0f8b7c2b9fcb839132c2b7ff4f2a9c3a6f3f0
Mike Cha <80453440+MckCha@users.noreply.github.com>
2025-09-20 13:05:14 -0700

    chore: initial project setup with base template

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
