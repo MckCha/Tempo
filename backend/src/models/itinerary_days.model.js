adc5e2f7173c63ac16e26cefca860edf63149beb
Mike Cha <80453440+MckCha@users.noreply.github.com>
2025-11-06 20:26:04 -0800

    feat(itinerary_days): add ItineraryDay model aligned with DB schema
    - add itineraries.model.js
    - properties: id, itinerary_id, day_number
    - no API behavior changes

---
+++ backend/src/models/itinerary_days.model.js
@@ -0,0 +1,10 @@
+
+class ItineraryDay {
+    constructor(id, itinerary_id, day_number) {
+        this.id = id;
+        this.itinerary_id = itinerary_id;
+        this.day_number = day_number;
+    }
+}
+
+export default ItineraryDay;
